b337bf4ef5e07e37c9080ab8441570bf98d9e2b1
K.D. Hubbard <hubbard.kd@gmail.com>
2023-11-17 15:06:15 -0700

    wip csv read

--- openai_service.rb
+++ openai_service.rb
@@ -1,11 +1,50 @@
-require 'faraday'
+require 'csv'
 
 class OpenAIService 
+  def categorize_prompt(csv)
+    product_info = CSV.parse(File.read(csv), headers: true)
+    product_info.each do |product|
+      product["product_name"] +
+    end
+    prompt = 
+    [
+      {
+        "role": "system",
+        "content": "You are a helpful assistant."
+      },
+      {
+        "role": "user",
+        "content": "Can you create categories for the following products based on their name and marketing copy?"
+      },
+      {
+          "role": "assistant",
+          "content": "Certainly! I'd be happy to help you create categories for the products based on their names and marketing copy. Please provide me with the names and marketing copy for the products, and I'll assist you in creating relevant categories."
+      },
+      {
+          "role": "user",
+          "content": "#{product_info}"
+    }
+    ]
+    p product_info[0]["product_name"]
+  end
 
   def conn
-    Faraday.new(url: 'https://api.openai.com/v1/chat/completions') do |faraday|
-      faraday.header['Authorization'] = ENV['SPOON-KEY']
-      faraday.params['number'] = 10
+    params = 
+    Faraday.post(url: 'https://api.openai.com/v1/chat/completions') do |faraday|
+      faraday.header['Authorization'] = ENV['OPENAI_KEY']
+      faraday.body = JSON.generate(params)
     end
   end
+
+  # def get_url
+  #   response = conn.get
+  #   JSON.parse(response.body)
+  # end
+
+  # def get_categories(csv)
+  #   get_url
+  # end
+
 end
+
+OpenAIService.new.categorize_prompt("/Users/kdhubbard/mod_5/hubbard_smarterxassessment/smarterx_data_sample.csv")
